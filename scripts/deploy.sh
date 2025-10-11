aa0c9ccd97edbddeee36092ed7707bbf58d7fe25
Lykd <lykd@pm.me>
2025-10-11 15:43:52 +0300

    Added anki with heatmap

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
+cp /home/lyka/heatmap.json static/
 rsync -avz --delete --no-o --no-g _site/ root@23.94.5.170:/srv/shoggothstaring.com/ &&
   ssh root@23.94.5.170 "chown -R caddy:caddy /srv/shoggothstaring.com" &&
   echo "Sync and ownership fixed." || echo "Sync failed."
